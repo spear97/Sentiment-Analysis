2d817cd6b8ec108397e72387e7c99893f60bf739
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 09:23:42 -0600

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,26 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+// Function to run sentiment analysis
+let RunSentimentAnalysis = () => {
+    // Get the value of the input field with id "textToAnalyze"
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
 
+    // Create a new XMLHttpRequest object
     let xhttp = new XMLHttpRequest();
+
+    // Define a function to handle the response
     xhttp.onreadystatechange = function() {
+        // Check if the request is complete and successful
         if (this.readyState == 4 && this.status == 200) {
+            // Update the content of the element with id "system_response"
             document.getElementById("system_response").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "sentimentAnalyzer?textToAnalyze"+"="+textToAnalyze, true);
+
+    // Construct the URL for the GET request
+    let url = "sentimentAnalyzer?textToAnalyze=" + encodeURIComponent(textToAnalyze);
+
+    // Open a new GET request
+    xhttp.open("GET", url, true);
+
+    // Send the request
     xhttp.send();
-}
+};
