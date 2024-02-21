067cadc1d0ed110a553d4495ae407fc6f9a19339
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 09:25:28 -0600

    Update sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,16 +1,33 @@
-import requests
-import json
+# Import necessary libraries
+import requests  # Library for making HTTP requests
+import json  # Library for working with JSON data
 
-def sentiment_analyzer(text_to_analyse):
+# Function to perform sentiment analysis
+def sentiment_analyzer(text_to_analyze):
+    # URL of the sentiment analysis service
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    
+    # Data to be sent in the POST request
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    # Headers for the POST request
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    
+    # Send POST request to the sentiment analysis service
+    response = requests.post(url, json=myobj, headers=header)
+    
+    # Parse the JSON response
     formatted_response = json.loads(response.text)
+    
+    # Check the status code of the response
     if response.status_code == 200:
+        # If successful (status code 200), extract label and score
         label = formatted_response['documentSentiment']['label']
         score = formatted_response['documentSentiment']['score']
     elif response.status_code == 500:
+        # If internal server error (status code 500), set label and score to None
         label = None
         score = None
+    
+    # Return a dictionary with label and score
     return {'label': label, 'score': score}
